498030251d37442b33c3385c01a79c7bed475c53
Shikhar Mattoo <matooshikhar@gmail.com>
2025-08-05 13:57:24 -0500

    removed remove citations, ame it optional

--- ai_ta_backend/service/project_service.py
+++ ai_ta_backend/service/project_service.py
@@ -65,7 +65,7 @@ class ProjectService:
           "system_prompt": None,
           "disabled_models": None,
           "project_description": project_description if project_description else None,
-          "removeCitations": False,
+          #"removeCitations": False,
       }
 
       # Set course_metadatas
